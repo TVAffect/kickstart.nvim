851a96d44fc0845e31a4a31144e4bae3b49fe48f
TVAffect <kemmi.lolstop@gmail.com>
2026-02-21 14:14:57 +0300

    dms clipboard

--- init.lua
+++ init.lua
@@ -84,6 +84,19 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+vim.g.clipboard = {
+  name = 'dms',
+  copy = {
+    ['+'] = { 'dms', 'cl', 'copy' },
+    ['*'] = { 'dms', 'cl', 'copy' },
+  },
+  paste = {
+    ['+'] = { 'dms', 'cl', 'paste' },
+    ['*'] = { 'dms', 'cl', 'paste' },
+  },
+  cache_enabled = 0,
+}
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
